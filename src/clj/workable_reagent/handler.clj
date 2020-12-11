c94856fd97e373fb739a7eb6b20eae391319b918
Pablo Sanchez Alonso <pablo.sanchez@codurance.com>
2020-12-11 14:12:13 +0000

    Added stage handler to communicate with Workable

--- src/clj/workable_reagent/handler.clj
+++ src/clj/workable_reagent/handler.clj
@@ -3,7 +3,9 @@
    [reitit.ring :as reitit-ring]
    [workable-reagent.middleware :refer [middleware]]
    [hiccup.page :refer [include-js include-css html5]]
-   [config.core :refer [env]]))
+   [config.core :refer [env]]
+   [workable-reagent.workable :refer [get-stages]]
+   [workable-reagent.config :refer [default-config]]))
 
 (def mount-target
   [:div#app
@@ -32,10 +34,22 @@
    :headers {"Content-Type" "text/html"}
    :body (loading-page)})
 
+(defn test-handler [_request]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body "{\"lol\": true }"})
+
+(defn stages-handler [_request]
+  (let [workable-response (get-stages default-config)]
+    {:status (:status workable-response)
+     :body (:body workable-response)}))
+
 (def app
   (reitit-ring/ring-handler
    (reitit-ring/router
-    [["/" {:get {:handler index-handler}}]])
+    [["/" {:get {:handler index-handler}}]
+     ["/lol" {:get {:handler test-handler}}]
+     ["/api/stages" {:get {:handler stages-handler}}]])
    (reitit-ring/routes
     (reitit-ring/create-resource-handler {:path "/" :root "/public"})
     (reitit-ring/create-default-handler))
